fdf07b510d096780425f5420ba7948e49dc551be
Roman Staněk <rnsk@seznam.cz>
2019-03-17 17:13:03 +0100

    Initial commit

--- ChessTracking/Forms/MainGameForm.cs
+++ ChessTracking/Forms/MainGameForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ChessTracking
+namespace ChessTracking.Forms
 {
-    public partial class Form1 : Form
+    public partial class MainGameForm : Form
     {
-        public Form1()
+        public MainGameForm()
         {
             InitializeComponent();
         }
--- ChessTracking/Program.cs
+++ ChessTracking/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChessTracking.Forms;
 
 namespace ChessTracking
 {
@@ -16,7 +17,7 @@ namespace ChessTracking
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainGameForm());
         }
     }
 }
