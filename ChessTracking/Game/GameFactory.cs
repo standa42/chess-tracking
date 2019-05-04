c22c641abb9fa6d55e9963f5336c81b060504bc6
Roman Staněk <rnsk@seznam.cz>
2019-05-04 18:09:56 +0200

    Game Factory added

---
+++ ChessTracking/Game/GameFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTracking.Game
+{
+    public static class GameFactory
+    {
+        public static GameData NewGame()
+        {
+
+        }
+
+    }
+}
