d25463be11408cd04b1df1db2e97111f5637d416
Roman Staněk <rnsk@seznam.cz>
2019-05-04 10:59:37 +0200

    Old visualiser removed

--- ChessTracking/ProcessingPipeline/Pipeline.cs
+++ ChessTracking/ProcessingPipeline/Pipeline.cs
@@ -20,7 +20,6 @@ namespace ChessTracking.ProcessingPipeline
     {
         public BlockingCollection<Message> ProcessingOutputQueue { get; }
         public VisualisationType VisualisationType { get; set; }
-        public AttemptToRecreateOldVisualiser OldVisualiser { get; }
         private Bitmap VisualisationBitmap { get; set; }
         private bool IsTracking { get; set; }
         private PlaneLocalization PlaneLocalization { get; set; }
@@ -31,7 +30,6 @@ namespace ChessTracking.ProcessingPipeline
         public Pipeline(BlockingCollection<Message> processingOutputQueue)
         {
             ProcessingOutputQueue = processingOutputQueue;
-            OldVisualiser = new AttemptToRecreateOldVisualiser(processingOutputQueue);
             VisualisationType = VisualisationType.RawRGB;
             IsTracking = false;
             PlaneLocalization = new PlaneLocalization(this);
@@ -49,7 +47,6 @@ namespace ChessTracking.ProcessingPipeline
             var rawData = new RawData(resources, VisualisationType);
             if (!IsTracking)
             {
-                Preprocessing(rawData);
                 var planeData = PlaneLocalization.Recalibrate(rawData);
                 var chessboardData = ChessboardLocalization.Recalibrate(planeData);
                 var figuresData = FiguresLocalization.Recalibrate(chessboardData);
@@ -60,7 +57,6 @@ namespace ChessTracking.ProcessingPipeline
                 Semaphore.Wait();
                 Task.Run(() =>
                 {
-                    Preprocessing(rawData);
                     var planeData = PlaneLocalization.Track(rawData);
                     var chessboardData = ChessboardLocalization.Track(planeData);
                     var figuresData = FiguresLocalization.Track(chessboardData);
@@ -71,52 +67,8 @@ namespace ChessTracking.ProcessingPipeline
                 });
             }
 
-            //OldVisualiser.Update(
-            //    rawData.ColorFrameData,
-            //    rawData.DepthData,
-            //    rawData.InfraredData,
-            //    rawData.CameraSpacePointsFromDepthData,
-            //    rawData.PointsFromColorToDepth,
-            //    rawData.PointsFromDepthToColor
-            //);
-
-            //Bitmap bmp = new Bitmap(1920, 1080, PixelFormat.Format32bppArgb);
-            //BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0,
-            //        bmp.Width,
-            //        bmp.Height),
-            //    ImageLockMode.WriteOnly,
-            //    bmp.PixelFormat);
-
-            //IntPtr pNative = bmpData.Scan0;
-            //Marshal.Copy(rawData.ColorFrameData, 0, pNative, rawData.ColorFrameData.Length);
-
-            //bmp.UnlockBits(bmpData);
-
-            //SendResultMessage(new ResultMessage(bmp, null));
         }
-        private StreamWriter sw = new StreamWriter(@"D:\Desktop\log.txt");
-        private void Preprocessing(RawData rawData)
-        {
-            // TODO: přemístit jinam
-            //rawData.ColorFrameData = rawData.ColorFrameData.FlipHorizontally(1920, 1080, 4);
-            //rawData.DepthData = rawData.DepthData.FlipHorizontally(512, 424);
-            //rawData.InfraredData = rawData.InfraredData.FlipHorizontally(512, 424);
-            //rawData.CameraSpacePointsFromDepthData = rawData.CameraSpacePointsFromDepthData.FlipHorizontally(512, 424);
-            //rawData.PointsFromColorToDepth = rawData.PointsFromColorToDepth.FlipHorizontally(1920, 1080);
-            //rawData.PointsFromDepthToColor = rawData.PointsFromDepthToColor.FlipHorizontally(512, 424);
-
-            //for (int i = 0; i < rawData.CameraSpacePointsFromDepthData.Length; i++)
-            //{
-            //    rawData.CameraSpacePointsFromDepthData[i].X = -rawData.CameraSpacePointsFromDepthData[i].X;
-            //    rawData.PointsFromDepthToColor[i].X = 1920 - rawData.PointsFromDepthToColor[i].X;
-            //}
-
-            //for (int i = 0; i < rawData.PointsFromColorToDepth.Length; i++)
-            //{
-            //    rawData.PointsFromColorToDepth[i].X = 512 - rawData.PointsFromColorToDepth[i].X;
-            //}
-        }
-
+        
         public void SetVisualisationBitmap(Bitmap bm)
         {
             VisualisationBitmap = bm;
@@ -125,7 +77,6 @@ namespace ChessTracking.ProcessingPipeline
         public void Recalibrate()
         {
             IsTracking = false;
-            //OldVisualiser.Recalibrate();
         }
 
         private void SendResultMessage(Message msg)
