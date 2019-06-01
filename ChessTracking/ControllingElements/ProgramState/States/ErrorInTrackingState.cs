a21a437b56365af5c2571a3f659e9e709fdd5e13
Roman Staněk <rnsk@seznam.cz>
2019-06-01 11:45:06 +0200

    Added Error in tracking state

---
+++ ChessTracking/ControllingElements/ProgramState/States/ErrorInTrackingState.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTracking.ControllingElements.ProgramState.States
+{
+    class ErrorInTrackingState
+    {
+        private StateContext StateContext { get; }
+
+        public ErrorInTrackingState(StateContext stateContext)
+        {
+            StateContext = stateContext;
+        }
+
+        public void GameLoaded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GameEnded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartedTracking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoppedTracking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Recalibrating()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GameFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorInTracking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TrackingStartSuccessful()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
