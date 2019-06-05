bcf5fc4dd32536f4dbac306773f621d5e1840a41
Roman Staněk <rnsk@seznam.cz>
2019-06-05 23:47:17 +0200

    Material design - blue theme

--- ChessTracking/UserInterface/MainGameForm.cs
+++ ChessTracking/UserInterface/MainGameForm.cs
@@ -46,7 +46,7 @@ namespace ChessTracking.UserInterface
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightBlue400, Primary.LightBlue600, Primary.Red100, Accent.Pink100, TextShade.WHITE);
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue300, Primary.Blue500, Primary.Red100, Accent.Pink100, TextShade.WHITE);
            
         }
 
