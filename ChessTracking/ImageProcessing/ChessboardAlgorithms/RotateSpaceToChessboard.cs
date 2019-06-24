3d30ce7e96c0dcceba44c531df577bf17b4d308d
Roman Staněk <rnsk@seznam.cz>
2019-06-24 17:58:18 +0200

    Refactior of chessboard base rotation

--- ChessTracking/ImageProcessing/ChessboardAlgorithms/RotateSpaceToChessboard.cs
+++ ChessTracking/ImageProcessing/ChessboardAlgorithms/RotateSpaceToChessboard.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Accord.Math;
 using ChessTracking.ImageProcessing.PlaneAlgorithms;
 using Microsoft.Kinect;
@@ -11,11 +7,11 @@ namespace ChessTracking.ImageProcessing.ChessboardAlgorithms
 {
     class RotateSpaceToChessboard : IRotateSpaceToChessboard
     {
-        public void Rotate(Chessboard3DReprezentation boardRepprezentation, CameraSpacePoint[] cspFromdd)
+        public void Rotate(Chessboard3DReprezentation boardRepresentation, CameraSpacePoint[] csp)
         {
-            var firstVector = boardRepprezentation.FieldVector1;
-            var secondVector = boardRepprezentation.FieldVector2;
-            var cornerPoint = boardRepprezentation.CornerOfChessboard;
+            var firstVector = boardRepresentation.FieldVector1;
+            var secondVector = boardRepresentation.FieldVector2;
+            var cornerPoint = boardRepresentation.CornerOfChessboard;
 
             firstVector = MyVector3DStruct.Normalize(firstVector);
             secondVector = MyVector3DStruct.Normalize(secondVector);
@@ -26,29 +22,29 @@ namespace ChessTracking.ImageProcessing.ChessboardAlgorithms
             var yVec = new MyVector3DStruct();
             var zVec = new MyVector3DStruct();
             
-            // get new base based on cross product direction
-            if ((a.z > 0 && b.z < 0)) // puvodne (a.z > 0 && b.z < 0)
+            // get new base based on two known vectors and their cross product
+            if ((a.z > 0 && b.z < 0)) // originally (a.z > 0 && b.z < 0)
             {
                 zVec = a; // a
                 yVec = MyVector3DStruct.Normalize(MyVector3DStruct.CrossProduct(ref xVec, ref zVec));
 
             }
-            else if ((a.z < 0 && b.z > 0)) // puvodne (a.z < 0 && b.z > 0)
+            else if ((a.z < 0 && b.z > 0)) // originally (a.z < 0 && b.z > 0)
             {
                 zVec = b; // b
                 yVec = MyVector3DStruct.Normalize(MyVector3DStruct.CrossProduct(ref xVec, ref zVec));
             }
             else
             {
-                throw new OutOfMemoryException();
+                throw new InvalidOperationException();
             }
 
+            // normalize base
             xVec = MyVector3DStruct.Normalize(firstVector);
             yVec = MyVector3DStruct.Normalize(secondVector);
             zVec = MyVector3DStruct.Normalize(zVec);
 
-            // spočítat inverzní matici
-
+            // get inverse mapping to base
             double[,] matrix =
             {
 
@@ -58,15 +54,18 @@ namespace ChessTracking.ImageProcessing.ChessboardAlgorithms
             };
             var inverseMatrix = matrix.Inverse();
 
-            for (int i = 0; i < cspFromdd.Length; i++)
+            // move all points
+            for (int i = 0; i < csp.Length; i++)
             {
-                var nx = (float)(cspFromdd[i].X - cornerPoint.x);
-                var ny = (float)(cspFromdd[i].Y - cornerPoint.y);
-                var nz = (float)(cspFromdd[i].Z - cornerPoint.z);
+                // translation to corner point
+                var nx = (float)(csp[i].X - cornerPoint.x);
+                var ny = (float)(csp[i].Y - cornerPoint.y);
+                var nz = (float)(csp[i].Z - cornerPoint.z);
 
-                cspFromdd[i].X = (float)(inverseMatrix[0, 0] * nx + inverseMatrix[0, 1] * ny + inverseMatrix[0, 2] * nz);
-                cspFromdd[i].Y = (float)(inverseMatrix[1, 0] * nx + inverseMatrix[1, 1] * ny + inverseMatrix[1, 2] * nz);
-                cspFromdd[i].Z = (float)(inverseMatrix[2, 0] * nx + inverseMatrix[2, 1] * ny + inverseMatrix[2, 2] * nz);
+                // rotation around it to given base
+                csp[i].X = (float)(inverseMatrix[0, 0] * nx + inverseMatrix[0, 1] * ny + inverseMatrix[0, 2] * nz);
+                csp[i].Y = (float)(inverseMatrix[1, 0] * nx + inverseMatrix[1, 1] * ny + inverseMatrix[1, 2] * nz);
+                csp[i].Z = (float)(inverseMatrix[2, 0] * nx + inverseMatrix[2, 1] * ny + inverseMatrix[2, 2] * nz);
             }
         }
     }
